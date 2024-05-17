50d7624d976bb78be962772ff5d620a6b09357c4
Gracjan Antonowicz <74455009+antonowiczcz@users.noreply.github.com>
2024-05-17 08:13:43 +0200

    Delete Animacja-stoper directory

--- Animacja-stoper/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.animacjastoper {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.animacjastoper to javafx.fxml;
-    exports com.example.animacjastoper;
-}
